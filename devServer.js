f6e929a39964be0fb371d1b7c7ae9bd312bd7ebd
John Gruen <john.gruen@gmail.com>
2016-10-13 11:21:14 +0200

    change port handler

--- devServer.js
+++ devServer.js
@@ -5,6 +5,8 @@ var favicon = require('serve-favicon');
 
 var app = express();
 
+app.set('port', (process.env.PORT || 7770));
+
 app.use(express.static('dist'));
 app.use(favicon(__dirname + '/dist/images/favicon.ico'));
 
@@ -12,13 +14,12 @@ app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-var port = process.env.PORT || 7770;
 
-app.listen(port, 'localhost', function(err) {
+app.listen(app.get('port'), 'localhost', function(err) {
   if (err) {
     console.log(err);
     return;
   }
 
-  console.log('Listening at http://localhost:7770');
+  console.log('Listening at ' + app.get('port'));
 });
