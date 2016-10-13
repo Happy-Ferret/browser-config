0a64eaf4fdbf5d2f92f088bd72c12cbc95231504
John Gruen <john.gruen@gmail.com>
2016-10-13 10:53:05 +0200

    deploying again

--- devServer.js
+++ devServer.js
@@ -1,26 +1,15 @@
 var path = require('path');
 var express = require('express');
-var webpack = require('webpack');
-var config = require('./webpack.config.dev');
 
 var app = express();
-var compiler = webpack(config);
 
-app.use(require('webpack-dev-middleware')(compiler, {
-  noInfo: true,
-  publicPath: config.output.publicPath
-}));
-
-app.use(express.static('images'));
-
-app.use(require('webpack-hot-middleware')(compiler));
+app.use(express.static('dist'));
 
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
 
 var port = process.env.PORT || 7770;
-console.log(port);
 
 app.listen(port, 'localhost', function(err) {
   if (err) {
