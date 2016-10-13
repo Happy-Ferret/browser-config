dae40fd9096a482d8b8df13f37903819ddd338ef
John Gruen <john.gruen@gmail.com>
2016-10-13 11:34:51 +0200

    fix stuff

--- devServer.js
+++ devServer.js
@@ -15,11 +15,6 @@ app.get('/', function(req, res) {
 });
 
 
-app.listen(app.get('port'), 'localhost', function(err) {
-  if (err) {
-    console.log(err);
-    return;
-  }
-
-  console.log('Listening at ' + app.get('port'));
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
 });
